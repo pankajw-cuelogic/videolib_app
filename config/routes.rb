f3b478833748ff9bbbc231533c776bf0e46e86ed
pankajw-cuelogic <pankaj.wakchaure@cuelogic.com>
2017-07-03 15:52:05 +0530

    reverted all codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'video/index'
+  # get 'video/index'
 
   devise_for :users
+  resources :user , only:[:index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 #resources :video
-root 'video#index'
+root 'user#index'
+match '/new', to: 'user#new', via: :get
+match '/user/new', to: 'user#new', via: :get
+
 end
