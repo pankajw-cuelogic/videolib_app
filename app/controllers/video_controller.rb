2ab8cd207aabc8a6a6112d94529aa040c7cde4a1
pankajw-cuelogic <pankaj.wakchaure@cuelogic.com>
2017-07-03 11:25:34 +0530

    Completed till devise gem

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,4 +1,17 @@
 class VideoController < ApplicationController
-  def index
-  end
+	#before_action :set_post, only:[:show, :edit, :update, :destroy]
+	#before_action :authenticate_user, except: [:index, :show]
+
+	def index
+	end
+
+	def show  	
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
 end
