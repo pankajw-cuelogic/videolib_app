8236a725db7be3dceff1bcdc57d8299c3e640c61
pankajw-cuelogic <pankaj.wakchaure@cuelogic.com>
2017-07-04 10:39:05 +0530

    completed till cued operations

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # get 'video/index'
 
   devise_for :users, controllers: {registrations: 'registrations'}
-  resources :user , only:[:index, :show, :edit, :update, :destroy]
+  resources :user 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 #resources :video
 root 'video#index'
-#match '/new', to: 'user#new', via: :get
-#match '/user/new', to: 'user#new', via: :get
+match '/list', to: 'user#index', via: :get
+match '/new', to: 'user#new', via: :get
 
 end
