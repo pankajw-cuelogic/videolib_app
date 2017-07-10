a2eb61d11a4e9e5fac12370f224083b72620314a
pankajw-cuelogic <pankaj.wakchaure@cuelogic.com>
2017-07-10 12:42:12 +0530

    Implemented CRUD operations for category

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,10 @@ class ApplicationController < ActionController::Base
 rescue_from CanCan::AccessDenied do |exception|
       respond_to do |format|
         format.json { head :forbidden, content_type: 'text/html' }
-        format.html { redirect_to main_app.root_url, notice: exception.message }
+       # To redirect to root url if not authorised user
+       # format.html { redirect_to main_app.root_url, notice: exception.message }
+        # to redirect to same list page if not authorised user
+        format.html { redirect_to list_path, notice: exception.message }
         format.js   { head :forbidden, content_type: 'text/html' }
       end
     end
